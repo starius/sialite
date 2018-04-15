a373728eedaf29e96ba2a638cb6faad58d13ed02
Boris Nagaev <bnagaev@gmail.com>
2018-04-15 22:21:31 +0200

    add tool download

---
+++ download/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"github.com/NebulousLabs/Sia/modules"
+	"github.com/NebulousLabs/Sia/types"
+	"github.com/NebulousLabs/fastrand"
+	"github.com/starius/sialite/netlib"
+	"github.com/xtaci/smux"
+)
+
+var (
+	source = flag.String("source", "", "Source of data (siad node)")
+)
+
+type tee struct {
+	impl io.ReadWriter
+	sink io.Writer
+}
+
+func (t *tee) Read(b []byte) (int, error) {
+	n, err := t.impl.Read(b)
+	if err != nil {
+		return n, err
+	}
+	n1, err := t.sink.Write(b[:n])
+	if err != nil {
+		return n, err
+	} else if n1 != n {
+		log.Println(n1, n)
+		return n, io.ErrShortWrite
+	}
+	return n, nil
+}
+
+func (t *tee) Write(b []byte) (int, error) {
+	return t.impl.Write(b)
+}
+
+func main() {
+	flag.Parse()
+	node := *source
+	if node == "" {
+		i := fastrand.Intn(len(modules.BootstrapPeers))
+		node = string(modules.BootstrapPeers[i])
+	}
+	conn, err := netlib.Connect(node)
+	if err != nil {
+		panic(err)
+	}
+	sess, err := smux.Client(conn, nil)
+	if err != nil {
+		panic(err)
+	}
+	bchan := make(chan *types.Block)
+	go func() {
+		for range bchan {
+		}
+	}()
+	f := func() (io.ReadWriter, error) {
+		rw, err := sess.OpenStream()
+		if err != nil {
+			return nil, err
+		}
+		return &tee{
+			impl: rw,
+			sink: os.Stdout,
+		}, nil
+	}
+	if err := netlib.DownloadAllBlocks(bchan, f); err != nil {
+		panic(err)
+	}
+}
