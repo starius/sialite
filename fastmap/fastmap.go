70f4d2c35fa1501bb82d09d07bbca6e77bf6cb5a
Boris Nagaev <bnagaev@gmail.com>
2018-04-29 23:41:04 +0200

    fastmap: func -> class

--- fastmap/fastmap.go
+++ fastmap/fastmap.go
@@ -7,107 +7,126 @@ import (
 	"sort"
 )
 
-func Build(pageLen, keyLen, valueLen, prefixLen int, sortedInput io.Reader, data, index io.Writer) error {
+type Writer struct {
+	pageLen, keyLen, valueLen, prefixLen int
+
+	data, index io.Writer
+
+	valuesStart int
+	prevKey     []byte
+	page        []byte
+	prevPage    []byte
+	keyStart    int
+	valueStart  int
+	npages      int
+	hasPrevPage bool
+}
+
+func New(pageLen, keyLen, valueLen, prefixLen int, data, index io.Writer) (*Writer, error) {
 	perPage := pageLen / (keyLen + valueLen)
 	valuesStart := perPage * keyLen
-	rec := make([]byte, keyLen+valueLen)
-	key := rec[:keyLen]
-	value := rec[keyLen:]
-	prevKey := make([]byte, keyLen)
-	page := make([]byte, pageLen)
-	prevPage := make([]byte, pageLen)
-	hasPrevPage := false
-	keyStart := 0
-	valueStart := valuesStart
-	npages := 0
-	for {
-		if _, err := io.ReadFull(sortedInput, rec); err == io.EOF {
-			break
-		} else if err != nil {
-			return err
-		}
-		if c := bytes.Compare(prevKey, key); c == 0 {
-			// Skip duplicats.
-			// TODO return error
-			continue
-		} else if c == 1 {
-			return fmt.Errorf("Input is not ordered")
-		}
-		copy(prevKey, key)
-		if hasPrevPage {
-			// Move some records from prevPage to this page to have prefixes
-			// different for last of prevPage and first of this page.
-			remove := 0
-			for k := keyStart - keyLen; k >= 0; k -= keyLen {
-				if !bytes.Equal(prevPage[k:k+prefixLen], key[:prefixLen]) {
-					break
-				}
-				remove++
-			}
-			if remove*keyLen == keyStart {
-				return fmt.Errorf("Prefix is too short")
-			}
-			start := keyStart - remove*keyLen
-			n1 := copy(page, prevPage[start:keyStart])
-			keyStart = start
-			start = valueStart - remove*valueLen
-			n2 := copy(page[valuesStart:], prevPage[start:valueStart])
-			valueStart = start
-			// Fill empty slots with FF.
-			for i := keyStart; i < valuesStart; i++ {
-				prevPage[i] = 0xFF
-			}
-			for i := valueStart; i < pageLen; i++ {
-				prevPage[i] = 0xFF
-			}
-			if n, err := data.Write(prevPage); err != nil {
-				return err
-			} else if n != pageLen {
-				return io.ErrShortWrite
-			}
-			keyStart = n1
-			valueStart = valuesStart + n2
-		}
-		nextKeyStart := keyStart + keyLen
-		nextValueStart := valueStart + valueLen
-		copy(page[keyStart:nextKeyStart], key)
-		copy(page[valueStart:nextValueStart], value)
-		keyStart = nextKeyStart
-		valueStart = nextValueStart
-		if hasPrevPage || npages == 0 {
-			// Write prefix.
-			if n, err := index.Write(page[:prefixLen]); err != nil {
-				return err
-			} else if n != prefixLen {
-				return io.ErrShortWrite
-			}
-			npages++
-			hasPrevPage = false
-		}
-		if keyStart == valuesStart {
-			t := prevPage
-			prevPage = page
-			page = t
-			hasPrevPage = true
-		}
+	return &Writer{
+		pageLen:     pageLen,
+		keyLen:      keyLen,
+		valueLen:    valueLen,
+		prefixLen:   prefixLen,
+		data:        data,
+		index:       index,
+		valuesStart: valuesStart,
+		prevKey:     make([]byte, keyLen),
+		page:        make([]byte, pageLen),
+		prevPage:    make([]byte, pageLen),
+		valueStart:  valuesStart,
+	}, nil
+}
+
+func (w *Writer) Write(rec []byte) (int, error) {
+	if len(rec) != w.keyLen+w.valueLen {
+		return 0, fmt.Errorf("Wrong write size")
 	}
-	if hasPrevPage {
-		page = prevPage
+	key := rec[:w.keyLen]
+	value := rec[w.keyLen:]
+	if c := bytes.Compare(w.prevKey, key); c == 0 {
+		return 0, fmt.Errorf("Input has duplicates")
+	} else if c == 1 {
+		return 0, fmt.Errorf("Input is not ordered")
 	}
-	if keyStart != 0 {
+	copy(w.prevKey, key)
+	if w.hasPrevPage {
+		// Move some records from prevPage to this page to have prefixes
+		// different for last of prevPage and first of this page.
+		remove := 0
+		for k := w.keyStart - w.keyLen; k >= 0; k -= w.keyLen {
+			if !bytes.Equal(w.prevPage[k:k+w.prefixLen], key[:w.prefixLen]) {
+				break
+			}
+			remove++
+		}
+		if remove*w.keyLen == w.keyStart {
+			return 0, fmt.Errorf("Prefix is too short")
+		}
+		start := w.keyStart - remove*w.keyLen
+		n1 := copy(w.page, w.prevPage[start:w.keyStart])
+		w.keyStart = start
+		start = w.valueStart - remove*w.valueLen
+		n2 := copy(w.page[w.valuesStart:], w.prevPage[start:w.valueStart])
+		w.valueStart = start
+		// Fill empty slots with FF.
+		for i := w.keyStart; i < w.valuesStart; i++ {
+			w.prevPage[i] = 0xFF
+		}
+		for i := w.valueStart; i < w.pageLen; i++ {
+			w.prevPage[i] = 0xFF
+		}
+		if n, err := w.data.Write(w.prevPage); err != nil {
+			return 0, err
+		} else if n != w.pageLen {
+			return 0, io.ErrShortWrite
+		}
+		w.keyStart = n1
+		w.valueStart = w.valuesStart + n2
+	}
+	nextKeyStart := w.keyStart + w.keyLen
+	nextValueStart := w.valueStart + w.valueLen
+	copy(w.page[w.keyStart:nextKeyStart], key)
+	copy(w.page[w.valueStart:nextValueStart], value)
+	w.keyStart = nextKeyStart
+	w.valueStart = nextValueStart
+	if w.hasPrevPage || w.npages == 0 {
+		// Write prefix.
+		if n, err := w.index.Write(w.page[:w.prefixLen]); err != nil {
+			return 0, err
+		} else if n != w.prefixLen {
+			return 0, io.ErrShortWrite
+		}
+		w.npages++
+		w.hasPrevPage = false
+	}
+	if w.keyStart == w.valuesStart {
+		w.page, w.prevPage = w.prevPage, w.page
+		w.hasPrevPage = true
+	}
+	return len(rec), nil
+}
+
+func (w *Writer) Close() error {
+	if w.hasPrevPage {
+		w.page = w.prevPage
+	}
+	if w.keyStart != 0 {
 		// Partial page. Fill empty slots with FF.
-		for i := keyStart; i < valuesStart; i++ {
-			page[i] = 0xFF
+		for i := w.keyStart; i < w.valuesStart; i++ {
+			w.page[i] = 0xFF
 		}
-		for i := valueStart; i < pageLen; i++ {
-			page[i] = 0xFF
+		for i := w.valueStart; i < w.pageLen; i++ {
+			w.page[i] = 0xFF
 		}
-		// Page is full.
-		if n, err := data.Write(page); err != nil {
+		if n, err := w.data.Write(w.page); err != nil {
 			return err
-		} else if n != pageLen {
+		} else if n != w.pageLen {
 			return io.ErrShortWrite
 		}
+		w.keyStart = 0
 	}
 	return nil
 }
