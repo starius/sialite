49fa6e5bd939ff2dcbe042749033924a1f89922a
Boris Nagaev <bnagaev@gmail.com>
2018-05-05 07:26:52 +0200

    add wrk script to measure sialiteserver perf

---
+++ cache/sialiteserver/wrk-script.lua
@@ -0,0 +1,23 @@
+local counter = 1
+
+function setup(thread)
+   thread:set("id", counter)
+   counter = counter + 1
+end
+
+function init(args)
+    addresses = io.open("/tmp/addresses.txt")
+    filesize = addresses:seek("end")
+    if filesize % 77 ~= 0 then
+        error("bad file size")
+    end
+    n = filesize / 77
+    math.randomseed(os.time() + id)
+end
+
+function request()
+    r = math.random(0, n - 1)
+    addresses:seek("set", r * 77)
+    address = addresses:read(76)
+    return wrk.format(nil, '/v1/history?address=' .. address)
+end
