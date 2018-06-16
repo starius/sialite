ced69f28767cbfd2a8c3f406d138292aa373ad83
Boris Nagaev <bnagaev@gmail.com>
2018-06-16 19:30:58 +0200

    cache: add test with read data

---
+++ cache/cache_test.go
@@ -0,0 +1,189 @@
+package cache
+
+import (
+	"compress/gzip"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/NebulousLabs/Sia/types"
+)
+
+var (
+	cached1000Blocks []*types.Block
+	cachedAddresses  []string
+)
+
+func read1000Blocks() ([]*types.Block, error) {
+	if cached1000Blocks != nil {
+		return cached1000Blocks, nil
+	}
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return nil, fmt.Errorf("Unable to find current package file")
+	}
+	pkgDir := filepath.Dir(filename)
+	blocksFile := filepath.Join(pkgDir, "testdata", "first_1000.blocks.gz")
+	f, err := os.Open(blocksFile)
+	if err != nil {
+		return nil, fmt.Errorf("os.Open(%q): %v", blocksFile, err)
+	}
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		return nil, fmt.Errorf("gzip.NewReader: %v", err)
+	}
+	var blocks []*types.Block
+	for {
+		var block types.Block
+		err := encoding.ReadObject(gz, &block, types.BlockSizeLimit)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, fmt.Errorf("encoding.ReadObject: %v", err)
+		}
+		blocks = append(blocks, &block)
+	}
+	cached1000Blocks = blocks
+	return blocks, nil
+}
+
+func readAddresses() ([]string, error) {
+	if cachedAddresses != nil {
+		return cachedAddresses, nil
+	}
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return nil, fmt.Errorf("Unable to find current package file")
+	}
+	pkgDir := filepath.Dir(filename)
+	addressesFile := filepath.Join(pkgDir, "testdata", "addresses.txt")
+	f, err := os.Open(addressesFile)
+	if err != nil {
+		return nil, fmt.Errorf("os.Open(%q): %v", addressesFile, err)
+	}
+	defer f.Close()
+	var addresses []string
+	for {
+		var address string
+		if _, err := fmt.Fscanf(f, "%s", &address); err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, fmt.Errorf("fmt.Fscanf: %v", err)
+		}
+		addresses = append(addresses, address)
+	}
+	cachedAddresses = addresses
+	return addresses, nil
+}
+
+func TestOnRealBlocks(t *testing.T) {
+	blocks, err := read1000Blocks()
+	if err != nil {
+		t.Fatalf("read1000Blocks: %v", err)
+	}
+
+	cases := []struct {
+		memLimit                int
+		offsetLen               int
+		offsetIndexLen          int
+		addressPageLen          int
+		addressPrefixLen        int
+		addressFastmapPrefixLen int
+		addressOffsetLen        int
+	}{
+		{
+			memLimit:                1,
+			offsetLen:               8,
+			offsetIndexLen:          4,
+			addressPageLen:          4096,
+			addressPrefixLen:        16,
+			addressFastmapPrefixLen: 5,
+			addressOffsetLen:        4,
+		},
+		{
+			memLimit:                1,
+			offsetLen:               7,
+			offsetIndexLen:          2,
+			addressPageLen:          1500,
+			addressPrefixLen:        32,
+			addressFastmapPrefixLen: 3,
+			addressOffsetLen:        5,
+		},
+	}
+
+	addresses, err := readAddresses()
+	if err != nil {
+		t.Fatalf("readAddresses: %v", err)
+	}
+
+next:
+	for _, tc := range cases {
+		tmpDir, err := ioutil.TempDir("", "TestOnRealBlocks")
+		if err != nil {
+			t.Fatalf("ioutil.TempDir: %v", err)
+		}
+		b, err := NewBuilder(tmpDir, tc.memLimit, tc.offsetLen, tc.offsetIndexLen, tc.addressPageLen, tc.addressPrefixLen, tc.addressFastmapPrefixLen, tc.addressOffsetLen)
+		if err != nil {
+			t.Errorf("NewBuilder: %v", err)
+			continue next
+		}
+		for _, block := range blocks {
+			if err := b.Add(block); err != nil {
+				t.Errorf("b.Add: %v", err)
+				continue next
+			}
+		}
+		if err := b.Build(); err != nil {
+			t.Errorf("b.Build: %v", err)
+			continue next
+		}
+		s, err := NewServer(tmpDir)
+		if err != nil {
+			t.Errorf("NewServer: %v", err)
+			continue next
+		}
+	next2:
+		for _, address := range addresses {
+			addressBytes, err := hex.DecodeString(address)
+			if err != nil {
+				t.Errorf("hex.DecodeString(%s): %v", address, err)
+				continue next2
+			}
+			history, _, err := s.GetHistory(addressBytes[:32], "")
+			if err != nil {
+				t.Errorf("s.GetHistory(%s): %v", address, err)
+				continue next2
+			}
+			if len(history) == 0 {
+				t.Errorf("s.GetHistory(%s): returned nothing", address)
+				continue next2
+			}
+			// Change the first byte and make sure nothing is found.
+			addressBytes[0] = ^addressBytes[0]
+			history, _, err = s.GetHistory(addressBytes[:32], "")
+			if err != nil {
+				t.Errorf("s.GetHistory(%s): %v", hex.EncodeToString(addressBytes), err)
+				continue next2
+			}
+			if len(history) != 0 {
+				t.Errorf("s.GetHistory(%s): returned something", hex.EncodeToString(addressBytes))
+				continue next2
+			}
+		}
+		if err := s.Close(); err != nil {
+			t.Errorf("s.Close: %v", err)
+			continue next
+		}
+		if err := os.RemoveAll(tmpDir); err != nil {
+			t.Errorf("os.RemoveAll(%q): %v", tmpDir, err)
+			continue next
+		}
+	}
+}
