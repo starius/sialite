a2e1ab39b671f35b73a3110a74a6aad3b375376b
Boris Nagaev <bnagaev@gmail.com>
2018-07-05 01:13:02 +0200

    sialiteclient: factor out function addressHistory

--- cache/sialiteclient/main.go
+++ cache/sialiteclient/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/NebulousLabs/Sia/types"
 	"github.com/starius/sialite/cache"
 )
 
@@ -14,6 +15,68 @@ var (
 	server  = flag.String("server", "127.0.0.1:35813", "Target address")
 	address = flag.String("address", "", "Target address")
 )
+
+type fullItem struct {
+	source *cache.Item
+	payout *types.SiacoinOutput
+	tx     *types.Transaction
+}
+
+func addressHistory(address string, headers []byte) ([]fullItem, error) {
+	next := ""
+	var rawItems []cache.Item
+	for {
+		url := fmt.Sprintf("http://%s/v1/address-history?address=%s&start=%s", *server, address, next)
+		respHistory, err := http.Get(url)
+		if err != nil {
+			return nil, fmt.Errorf("http.Get(%q): %v", url, err)
+		}
+		if respHistory.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("http.Get(%q): %s", url, respHistory.Status)
+		}
+		var history []cache.Item
+		if err := encoding.NewDecoder(respHistory.Body).DecodeAll(&next, &history); err != nil {
+			return nil, fmt.Errorf("DecodeAll: %v", err)
+		}
+		respHistory.Body.Close()
+		rawItems = append(rawItems, history...)
+		if next == "" {
+			break
+		}
+	}
+	var fullItems []fullItem
+	for i := 0; i < len(rawItems); i++ {
+		item := &rawItems[i]
+		data, err := item.SourceData(nil)
+		if err != nil {
+			return nil, fmt.Errorf("item.SourceData: %v", err)
+		}
+		if item.Block < 0 || item.Block >= len(headers)/48 {
+			return nil, fmt.Errorf("bad block index: %d", item.Block)
+		}
+		header := headers[item.Block*48 : (item.Block+1)*48]
+		merkleRoot := header[16:]
+		if !cache.VerifyProof(merkleRoot, data, item.MerkleProof, item.Index, item.NumLeaves) {
+			return nil, fmt.Errorf("cache.VerifyProof: bad proof")
+		}
+		full := fullItem{source: item}
+		if item.Index < item.NumMinerPayouts {
+			var payout types.SiacoinOutput
+			if err := encoding.Unmarshal(data, &payout); err != nil {
+				return nil, fmt.Errorf("encoding.Unmarshal payout: %v", err)
+			}
+			full.payout = &payout
+		} else {
+			var tx types.Transaction
+			if err := encoding.Unmarshal(data, &tx); err != nil {
+				return nil, fmt.Errorf("encoding.Unmarshal tx: %v", err)
+			}
+			full.tx = &tx
+		}
+		fullItems = append(fullItems, full)
+	}
+	return fullItems, nil
+}
 
 func main() {
 	flag.Parse()
@@ -26,41 +89,15 @@ func main() {
 		panic(err)
 	}
 	respHeaders.Body.Close()
-	next := ""
-	var wholeHistory []cache.Item
-	for {
-		url := fmt.Sprintf("http://%s/v1/address-history?address=%s&start=%s", *server, *address, next)
-		respHistory, err := http.Get(url)
-		if err != nil {
-			panic(err)
-		}
-		if respHistory.StatusCode != http.StatusOK {
-			panic(respHistory.StatusCode)
-		}
-		var history []cache.Item
-		if err := encoding.NewDecoder(respHistory.Body).DecodeAll(&next, &history); err != nil {
-			panic(err)
-		}
-		respHistory.Body.Close()
-		wholeHistory = append(wholeHistory, history...)
-		if next == "" {
-			break
-		}
+	history, err := addressHistory(*address, headers)
+	if err != nil {
+		panic(err)
 	}
-	for _, item := range wholeHistory {
-		data, err := item.SourceData(nil)
-		if err != nil {
-			panic(err)
-		}
-		header := headers[item.Block*48 : (item.Block+1)*48]
-		merkleRoot := header[16:]
-		if !cache.VerifyProof(merkleRoot, data, item.MerkleProof, item.Index, item.NumLeaves) {
-			panic("bad proof")
-		}
-		if item.Index < item.NumMinerPayouts {
-			fmt.Println(item.Block, "miner_payout")
+	for _, full := range history {
+		if full.payout != nil {
+			fmt.Println(full.source.Block, "miner_payout")
 		} else {
-			fmt.Println(item.Block, "transaction")
+			fmt.Println(full.source.Block, "transaction")
 		}
 	}
 }
