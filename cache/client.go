b37842d153b3171a502cbb15db88179d657a0e6a
Pavel Dolgov <pdolgov99@gmail.com>
2018-07-07 16:16:25 +0100

    Implement calculateBlockTotals()

--- cache/client.go
+++ cache/client.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"bytes"
 	"fmt"
+	"math/big"
 	"sort"
 
 	"github.com/NebulousLabs/Sia/crypto"
@@ -86,6 +87,43 @@ func getHeadersSlice(headers []byte) ([]types.BlockHeader, error) {
 		return nil, fmt.Errorf("ParentID of 2nd header is not GenesisID")
 	}
 	return headersSlice, nil
+}
+
+// calculateBlockTotals computes the new total time and total target
+// for the current block.
+func calculateBlockTotals(
+	currentHeight types.BlockHeight,
+	currentBlockID types.BlockID,
+	prevTotalTime int64,
+	parentTimestamp, currentTimestamp types.Timestamp,
+	prevTotalTarget, targetOfCurrentBlock types.Target,
+) (newTotalTime int64, newTotalTarget types.Target) {
+	// Reset the prevTotalTime to a delta of zero just before the hardfork.
+	//
+	// NOTICE: This code is broken, an incorrectly executed hardfork. The
+	// correct thing to do was to not put in these 3 lines of code. It is
+	// correct to not have them.
+	//
+	// This code is incorrect, and introduces an unfortunate drop in difficulty,
+	// because this is an uncompreesed prevTotalTime, but really it should be
+	// getting set to a compressed prevTotalTime. And, actually, a compressed
+	// prevTotalTime doesn't have much meaning, so this code block shouldn't be
+	// here at all. But... this is the code that was running for the block
+	// 135,000 hardfork, so this code needs to stay. With the standard
+	// constants, it should cause a disruptive bump that lasts only a few days.
+	//
+	// The disruption will be complete well before we can deploy a fix, so
+	// there's no point in fixing it.
+	if currentHeight == types.OakHardforkBlock-1 {
+		prevTotalTime = int64(types.BlockFrequency * currentHeight)
+	}
+
+	// For each value, first multiply by the decay, and then add in the new
+	// delta.
+	newTotalTime = (prevTotalTime * types.OakDecayNum / types.OakDecayDenom) + (int64(currentTimestamp) - int64(parentTimestamp))
+	newTotalTarget = prevTotalTarget.MulDifficulty(big.NewRat(types.OakDecayNum, types.OakDecayDenom)).AddDifficulties(targetOfCurrentBlock)
+
+	return newTotalTime, newTotalTarget
 }
 
 func VerifyBlockHeaders(headers []byte) error {
